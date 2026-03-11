7f7fac402e024fee4811bffb9c5bec48207fa115
RaseltheHansel <jraselthehansel@gmail.com>
2026-03-11 10:52:54 +0800

    complete bookmarkController

---
+++ server/src/routes/bookmarkRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import verifyToken from "../middleware/auth";
+import { toggleBookmark, getByBookmarks, checkBookmarks } from "../controllers/bookmarkController";
+
+
+const router = Router();
+
+router.get('/', verifyToken, getByBookmarks);
+router.get('/:listingId/check', verifyToken, checkBookmarks);
+router.post('/:listingId', verifyToken, toggleBookmark);
+
+
+
+
+
+export default router;
