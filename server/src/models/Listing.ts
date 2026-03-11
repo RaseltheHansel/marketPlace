aa38ec38ac4b721bb7de511786709e4b1a922604
RaseltheHansel <jraselthehansel@gmail.com>
2026-03-11 12:04:01 +0800

    complete backend of marketplace

--- server/src/models/Listing.ts
+++ server/src/models/Listing.ts
@@ -7,12 +7,12 @@ const listingSchema = new Schema<IListing>(
     title: {type: String, required: true },
     description: {type: String, required: true},
     price: {type: Number, required: true, min: 0},
-    category: {type: String, enum: ['Electronics', 'Clothing', 'Furniture', 'Books', 'Vehicles', 'Sports', 'Toys', 'Foods', 'Others'], required: true},
-    condition: {type: String, enum: ['new', 'Like-new', 'good', 'fair', 'poor'], required: true},
+    category: {type: String, enum: ['Electronics', 'Clothing', 'Furniture', 'Books', 'Vehicles', 'Sports', 'Toys', 'Food', 'Others'], required: true},
+    condition: {type: String, enum: ['new', 'like-new', 'good', 'fair', 'poor'], required: true},
     images: [{type: String}],
     location: {type: String, required: true},
     status: {type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending'},
-    views: {types: Number, default: 0},
+    views: {type: Number, default: 0},
     },
     {timestamps: true}
 );
